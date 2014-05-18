cf9776754f044055616ae153071398ecaa6a0704
edenferreria <thiago_eden@hotmail.com>
2014-05-18 01:28:30 -0300

    retirado __ de atributos para leitura mais fácil, adicionando projeto do pycharm para o .gitignore

--- grafos.py
+++ grafos.py
@@ -1,67 +1,31 @@
-__author__ = 'Eden Thiago Ferreira'
+author = 'Eden Thiago Ferreira'
 from collections import defaultdict
-from plano_cartesiano import *
 
 class _Vertice:
 
     def __init__(self,cod,dado):
-        self.__cod = cod
-        self.__dado = dado
-
-    @property
-    def cod(self):
-        return self.__cod
-
-    @property
-    def dado(self):
-        return self.__dado
-
-    @dado.setter
-    def dado(self,value):
-        self.__dado = value
-
-
+        self.cod = cod
+        self.dado = dado
 
 class _Arco:
 
     def __init__(self,ver_a,ver_b,peso):
-        self.__ver_a,self.__ver_b = ver_a,ver_b
-        self.__peso = peso
-
-    @property
-    def ver_a(self):
-        return self.__ver_a
-
-    @property
-    def ver_b(self):
-        return self.__ver_b
-
-    @property
-    def peso(self):
-        return self.__peso
-
-    @peso.setter
-    def peso(self,value):
-        self.__peso = value
+        self.ver_a,self.ver_b = ver_a,ver_b
+        self.peso = peso
 
 class Digrafo:
 
     def __init__(self):
-        self.__vertices = set()
-        self.__arcos    = defaultdict(list)
+        self.vertices = set()
+        self.arcos    = defaultdict(list)
 
-    @property
-    def vertices(self):
-        return self.__vertices
-
-    @property
-    def arcos(self):
-        return self.__arcos
+    def len(self):
+        return len(self.vertices)
 
     def add_vertice(self,dado):
-        ver = _Vertice(len(self.__vertices),dado)
-        self.__vertices.add(ver)
+        ver = _Vertice(len(self.vertices),dado)
+        self.vertices.add(ver)
 
     def add_arco(self,ver_a,ver_b,peso):
         arc = _Arco(ver_a,ver_b,peso)
-        self.__arcos[ver_a].append(arc)
+        self.arcos[ver_a].append(arc)
--- plano_cartesiano.py
+++ plano_cartesiano.py
@@ -1,27 +1,11 @@
-__author__ = 'Eden Thiago Ferreira'
+author = 'Eden Thiago Ferreira'
 from math import sqrt
 
 class Ponto:
 
     def __init__(self,x,y):
-        self.__x,self.__y = x, y
+        self.x,self.y = x, y
 
-    @property
-    def x(self):
-        return self.__x
-
-    @x.setter
-    def x(self,value):
-        self.__x = value
-
-    @property
-    def y(self):
-        return self.__y
-
-    @y.setter
-    def y(self,value):
-        self.__y = value
-
-    def __sub__(self, other):
+    def sub(self, other):
         """retorna a distancia entre o ponto instanciado ao outro"""
         return sqrt(((other.x - self.x) ** 2) + ((other.y - self.y) ** 2))
