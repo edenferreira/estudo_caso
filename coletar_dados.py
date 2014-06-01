d4bced479254520fb67b1b7c0505ac8de9fbe53c
edenferreria <thiago_eden@hotmail.com>
2014-06-01 17:23:08 -0300

    Bug coletar_dados.py

--- coletar_dados.py
+++ coletar_dados.py
@@ -49,14 +49,14 @@ def coletar_normal(repet):
 
 def imprimir_dados():
     con = Conexao()
-    d = con.get_tempos_dijkstra()
-    a = con.get_tempos_astar()
+    d = con.get_tempos_dijkstra_sem_caminho()
+    a = con.get_tempos_astar_sem_caminho()
     with open('dicionario_dados.txt','w') as arq:
-      print('Tempos dos Algoritmos',file=arq)
-      print('                    Dijkstra                 A*',file=arq)
+        print('Tempos dos Algoritmos',file=arq)
+        print('                    Dijkstra                 A*',file=arq)
     with open('dicionario_dados.txt','a') as arq:
-      for i in range(len(d['id'])):
-        for key in d:
-          if key != 'id':
-            print(key.rjust(15),' : ',str(d[key][i]).ljust(20),' : ', str(a[key][i]),file=arq)
-        print(file=arq)
+        for i in range(len(d['id'])):
+            for key in d:
+                if key != 'id':
+                    print(key.rjust(15),' : ',str(d[key][i]).ljust(20),' : ', str(a[key][i]),file=arq)
+            print(file=arq)
