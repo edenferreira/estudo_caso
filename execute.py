ce9b510b39fb41bde998b5dd6adbe5f5c089ddaf
edenferreira <thiago_eden@hotmail.com>
2014-06-02 14:15:21 -0300

    Update execute.py

--- execute.py
+++ execute.py
@@ -1,7 +1,12 @@
 author = 'Eden Thiago Ferreira'
 import sys
 from coletar_dados import *
+from time import sleep
 
 repet = int(sys.argv[1])
-coletar_normal(repet)
+for i in range(repet):
+    print(repet)
+    coletar_normal()
+    sleep(2)
+imprimir_dados()
 
