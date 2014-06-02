8763b9ce8152dcc2c1eec0e2607f5dc4ddca6634
edenferreira <thiago_eden@hotmail.com>
2014-06-02 14:14:57 -0300

    Update coletar_dados.py

--- coletar_dados.py
+++ coletar_dados.py
@@ -1,50 +1,48 @@
 author = 'Eden Thiago Ferreira'
+from statistics import *
 from random import *
 from grafos import *
 from caminhos_minimos import *
 from banco_dados import *
 
-def coletar_normal(repet):
+def coletar_normal():
     criar_se_nao_existe()
     con = Conexao()
-    for i in range(repet):
-        tam_grid = randint(800,1200)
-        grafo = gerar_aleatoriamente(randint(1000,1500), randint(1000,1500), tam_grid, tam_grid, uniform(0.1,2), con.get_ident())
+    _x = randint(1000,2225)
+    _y = randint(1000,2225)
+    grafo = gerar_aleatoriamente(_x, _y, _x-500, _y-500, uniform(0.1,2), con.get_ident())
+    print('grafo')
+    con.inserir_grafo(grafo)
 
-        con.inserir_grafo(grafo)
+    pt_o, pt_d = sample(grafo.pontos, 2)
+    dij = Dijkstra(grafo)
+    dij.executar(pt_o, pt_d)
+    ast = AStar(grafo)
+    ast.executar(pt_o, pt_d)
+    con.inserir_tabela_dijkstra(dij)
+    con.inserir_tabela_astar(ast)
+    del dij
+    del ast
 
-        pt_o, pt_d = sample(grafo.pontos, 2)
-        dij = Dijkstra(grafo)
-        dij.executar(pt_o, pt_d)
-        ast = AStar(grafo)
-        ast.executar(pt_o, pt_d)
-        con.inserir_tabela_dijkstra(dij)
-        con.inserir_tabela_astar(ast)
-        del dij
-        del ast
+    pt_o, pt_d = sample(grafo.pontos, 2)
+    dij = Dijkstra(grafo)
+    dij.executar(pt_o, pt_d)
+    ast = AStar(grafo)
+    ast.executar(pt_o, pt_d)
+    con.inserir_tabela_dijkstra(dij)
+    con.inserir_tabela_astar(ast)
+    del dij
+    del ast
 
-        pt_o, pt_d = sample(grafo.pontos, 2)
-        dij = Dijkstra(grafo)
-        dij.executar(pt_o, pt_d)
-        ast = AStar(grafo)
-        ast.executar(pt_o, pt_d)
-        con.inserir_tabela_dijkstra(dij)
-        con.inserir_tabela_astar(ast)
-        del dij
-        del ast
-
-        pt_o, pt_d = sample(grafo.pontos, 2)
-        dij = Dijkstra(grafo)
-        dij.executar(pt_o, pt_d)
-        ast = AStar(grafo)
-        ast.executar(pt_o, pt_d)
-        con.inserir_tabela_dijkstra(dij)
-        con.inserir_tabela_astar(ast)
-        del dij
-        del ast
-
-        imprimir_dados()
-
+    pt_o, pt_d = sample(grafo.pontos, 2)
+    dij = Dijkstra(grafo)
+    dij.executar(pt_o, pt_d)
+    ast = AStar(grafo)
+    ast.executar(pt_o, pt_d)
+    con.inserir_tabela_dijkstra(dij)
+    con.inserir_tabela_astar(ast)
+    del dij
+    del ast
     del con
 
 def imprimir_dados():
@@ -58,5 +56,8 @@ def imprimir_dados():
         for i in range(len(d['id'])):
             for key in d:
                 if key != 'id':
-                    print(key.rjust(15),' : ',str(d[key][i]).ljust(20),' : ', str(a[key][i]),file=arq)
+                    if key == 'distancia_total':
+                        print(key.rjust(15),' : ',str(round(d[key][i],3)).ljust(20),' : ', str(round(a[key][i],3)),file=arq)
+                    else:
+                        print(key.rjust(15),' : ',str(d[key][i]).ljust(20),' : ', str(a[key][i]),file=arq)
             print(file=arq)
