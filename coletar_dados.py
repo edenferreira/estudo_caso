67fa0740ec1df847b40fda18bc9c535e0d6ccafa
edenferreira <thiago_eden@hotmail.com>
2014-06-05 08:08:23 -0300

    Update coletar_dados.py

--- coletar_dados.py
+++ coletar_dados.py
@@ -1,9 +1,10 @@
 author = 'Eden Thiago Ferreira'
-from statistics import *
+#from statistics import *
 from random import *
 from grafos import *
 from caminhos_minimos import *
 from banco_dados import *
+from pprint import pprint
 
 def coletar_normal():
     criar_se_nao_existe()
@@ -49,10 +50,10 @@ def imprimir_dados():
     con = Conexao()
     d = con.get_tempos_dijkstra_sem_caminho()
     a = con.get_tempos_astar_sem_caminho()
-    with open(r'/var/ftp/pub/dicionario_dados.txt','w') as arq:
+    with open(r'dicionario_dados.txt','w') as arq:
         print('Tempos dos Algoritmos',file=arq)
         print('                    Dijkstra                 A*',file=arq)
-    with open(r'/var/ftp/pub/dicionario_dados.txt','a') as arq:
+    with open(r'dicionario_dados.txt','a') as arq:
         for i in range(len(d['id'])):
             for key in d:
                 if key != 'id':
@@ -60,4 +61,21 @@ def imprimir_dados():
                         print(key.rjust(15),' : ',str(round(d[key][i],3)).ljust(20),' : ', str(round(a[key][i],3)),file=arq)
                     else:
                         print(key.rjust(15),' : ',str(d[key][i]).ljust(20),' : ', str(a[key][i]),file=arq)
+            print(file=arq)
+
+def recuperar_dados():
+    con = Conexao()
+    d = con.get_tempos_dijkstra()
+    a = con.get_tempos_astar()
+    with open('dijkstra.txt','w') as arq:
+        for e in d:
+            for f in e:
+                if f != e[5]:
+                    print(f,end=',',file=arq)
+            print(file=arq)
+    with open('astar.txt','w') as arq:
+        for e in a:
+            for f in e:
+                if f != e[5]:
+                    print(f,end=',',file=arq)
             print(file=arq)
